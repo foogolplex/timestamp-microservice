a7616317ee354e6b9b63ad0b584593ba82928209
Robert Wood <robertwood@tuta.io>
2021-11-03 17:12:14 -0500

    Added procfile for heroku and added functionality.

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// In the case no parameter is passed return the current time
+app.get('/api/', (req, res) => {
+    var date = new Date(); // Offset +30000ms for freecodecamp test automation
+  
+    return res.json({
+        unix : date.getTime(),
+        utc : date.toUTCString()
+    });
+});
+
+// Endpoint for timestamps
+app.get('/api/:date', (req, res) => {
+    var dateObj;
+
+    // Get the date paramater string
+    date = new Date(req.params['date']);
+    // Convert it to a unix timestamp if invalid
+    if(date.toUTCString() == "Invalid Date"){
+      date = new Date(parseInt(req.params['date']));
+    }
+    // If date is still invalid return an error response
+    if(date.toUTCString() == "Invalid Date"){
+        return res.json({'error': 'Invalid Date'});
+    }
+    else{
+        dateObj = {
+            unix : date.getTime(),
+            utc : date.toUTCString()
+        };
+        return res.json(dateObj);
+    }
+});
+
 app.listen(port, () => {
-  console.log(`timestamp-microservice listening at http://localhost:${port}`)
+    console.log(`timestamp-microservice listening at http://localhost:${port}`)
 })
